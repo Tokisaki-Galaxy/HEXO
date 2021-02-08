3cea399f78a6bca38dc84cf0a849b595f9af919d
Tokisaki-Galaxy <tokisaki-galaxy@qq.com>
2021-02-08 12:33:55 +0800

    update

--- !push.bat
+++ !push.bat
@@ -1,16 +1,21 @@
 @echo off
 echo Enter a description of this push(Optional):
 set /p d=
+
+cd themes\hexo-theme-matery
+git add -A
+git commit -m "%d%"
+echo Upgrade hexo-theme-matery...
+git push||git push||git push
+
+cd ..\..
 git add -A
 git commit -m "%d%"
 git push||git push||git push
-echo Upgrade Res...
+echo Upgrade HEXO...
+
 cd ..\res
 git add -A
 git commit -m "%d%"
 git push||git push||git push
-echo Upgrade hexo-theme-matery...
-cd ..\HEXO\themes\hexo-theme-matery
-git add -A
-git commit -m "%d%"
-git push||git push||git push
+echo Upgrade Res...
