b6e46d45833b335dc806862dfe0cfa0e8426a064
Tokisaki-Galaxy <tokisaki-galaxy@qq.com>
2021-03-17 15:07:39 +0800

    update a post

--- !pull.bat
+++ !pull.bat
@@ -1,1 +1,13 @@
-@git pull
+@echo off
+
+echo Upgrade hexo-theme-matery...
+cd themes\hexo-theme-matery
+git pull||git pull||git pull
+
+echo Upgrade HEXO...
+cd ..\..
+git pull||git pull||git pull
+
+echo Upgrade Res...
+cd ..\res
+git pull||git pull||git pull
